30af835549d19ca0f80101d6d8395f03ccb19d1b
amol avlod <amolavlod328@gmail.com>
2025-07-26 01:11:16 +0530

    Add files via upload

---
+++ c module/assignment 0/arearec.c
@@ -0,0 +1,8 @@
+//Write a C program to input the base and height of a triangle and calculate its area.
+#include<stdio.h>
+void main(){
+	float base=10,hei=10,area;
+	
+	area= 0.5*base*hei;
+	printf("area is %f",area);
+}
