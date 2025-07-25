72870ff990ec7d5ea18e84d67c0349d6bd66b0f2
amol avlod <amolavlod328@gmail.com>
2025-07-26 01:12:43 +0530

    Add files via upload

---
+++ c module/assignment 0/avrg.c
@@ -0,0 +1,8 @@
+//Write a C program to input five numbers and find their average.
+#include<stdio.h>
+void main(){
+	float r1=1,r2=2,r3=3,r4=4,r5=5,avg;
+	
+	avg=(r1+r2+r3+r4+r5)/5;
+	printf("avg of %f",avg);
+}
