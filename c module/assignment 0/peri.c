3084f738be8a9734d52257131970b826879afeb9
amol avlod <amolavlod328@gmail.com>
2025-07-26 01:14:57 +0530

    Add files via upload

---
+++ c module/assignment 0/peri.c
@@ -0,0 +1,8 @@
+//Write a C program to input the length and width of a rectangle and find its perimeter.
+#include<stdio.h>
+void main(){
+	int len=5 ,wid=8,peri;
+	
+	peri= 2*(len + wid);
+	printf("peri is %d",peri);
+}
